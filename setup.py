778144cd4aa17829b2c98dfc62931c355fdca285
ojorgensen <ojorgensen1417@gmail.com>
2023-07-20 15:51:23 +0100

    Added a setup file, copying Alex Turner's

---
+++ setup.py
@@ -0,0 +1,41 @@
+# Shamelessly copied from Turner et al's repo for their Steering Vectors post
+
+from setuptools import setup, find_packages
+
+setup(
+    name="transformer-activation-exploration",
+    description=(
+        "Tools for investigating the activations of"
+        " language models"
+    ),
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[
+        (
+            "transformer-lens @"
+            " git+https://github.com/neelnanda-io/TransformerLens@9e034eef796ed32b0e473ca9f3c9d31e5d5046e7"
+        ),
+        "torch==1.13.1",
+        "numpy>=1.22.1",
+        "pandas>=1.4.4",
+        "jaxtyping>=0.2.14",
+        "prettytable>=3.6.0",
+        "openai>=0.27.2",
+        "nltk>=3.8.1",
+        "kaleido>=0.2.1",
+        "pytest",
+        "plotly",
+        "nbformat",
+        "Ipython",
+        "ipywidgets",
+        "tuned_lens",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "notebook",  # liked by vscode
+        ]
+    },
+)
